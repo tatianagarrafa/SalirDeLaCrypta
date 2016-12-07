091639427e208add1178636a0c697dc3b5c84df7
unknown <e1595070@FC411-MJPTCKT.cmaisonneuve.qc.ca>
2016-12-07 11:12:22 -0500

    choix de personnage

---
+++ Assets/scripts/choixPerso.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class choixPerso : MonoBehaviour {
+
+	// http://answers.unity3d.com/questions/894211/set-objects-child-to-activeinactive.html
+
+	private string choixPersonnage;
+	private int monChoix;
+
+	// Use this for initialization
+	void Start () {
+		choixPersonnage = PlayerPrefs.GetString ("choixPerso");
+		//monChoix = transform.Find (choixPersonnage);
+		//monChoix.enabled = true; 
+		if (choixPersonnage == "Nahua") {
+			monChoix = 1;
+		} else {
+			monChoix = 0;
+		}
+		this.transform.GetChild (monChoix).gameObject.SetActive (true);
+
+	}
+	
+
+}
